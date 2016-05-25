a89bec67bab13f036f13fa6644b65f19fd66f5fb
Stefan Karan <stefan.karan@ivana.karaulic-MacLap7>
2016-05-25 14:01:11 +0200

    bc 25.05.

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,28 +1,40 @@
 $(document).ready(function(){
-    $( ".cross" ).hide();
-    $( ".header__body-mobile" ).hide();
-    $( ".hamburger" ).click(function() {
-        $( ".header__body-mobile" ).fadeToggle( "fast", function() {
-        $( ".hamburger" ).hide();
-        $( ".cross" ).show();
-        });
-    });
-
-    $( ".cross" ).click(function() {
-        $( ".header__body-mobile" ).fadeToggle( "fast", function() {
-        $( ".cross" ).hide();
-        $( ".hamburger" ).show();
-        });
-    });
     
+    // Sticky header
 //    $(window).scroll(function() {
-//        if ($(this).scrollTop() > 1){
+//        if ($(this).scrollTop() > 240 && $(this).width() >= 751){
 //            $('header').addClass("header__sticky");
-//            $('header').removeClass("header");
+////            $('header').removeClass("header");
 //        }
 //        else{
 //            $('header').removeClass("header__sticky");
-//            $('header').addClass("header");
+////            $('header').addClass("header");
 //        }
 //    });
+
+    // Mobile hamb - open 
+    $( ".header__cross" ).hide();
+    
+    $(".header__hamburger").on("click", function(){
+        $(this).parent(".header__body").toggleClass("active");
+        $( ".header__hamburger" ).hide();
+        $( ".header__cross" ).show();
+    });
+    
+    // Mobile hamb - close 
+    $(".header__cross").on("click", function(){
+        $(this).parent(".header__body").toggleClass("active");
+        $( ".header__hamburger" ).show();
+        $( ".header__cross" ).hide();
+    });
+    
+    // Open submenu on mobile
+    $(document).ready(function(){
+        if (window.innerWidth <= 1024){
+            $('.sub__first').click(function() {
+                    $('.header__sub-first').slideToggle("slow").css('display','inline-block');
+                    $(".sub__first .header__menu-link").toggleClass("active");
+            });
+        }
+    });
 });
